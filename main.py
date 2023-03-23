264531e77717125175e6f5c5350ed956c1605c57
Krystian <krisor2002@gmail.com>
2023-03-23 11:07:52 +0100

    cipa z ohjo

---
+++ main.py
@@ -0,0 +1,15 @@
+string = input('wpisz liczbe')
+
+liczba=float(string)
+
+if liczba <10:
+    print(liczba, "jest mniejsze od 10")
+
+elif liczba == 10:
+ print('liczba jest rowna 10')
+
+else:
+ print(liczba, "jest wieksza od 10")
+
+
+
